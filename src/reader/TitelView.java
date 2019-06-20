2be5708747ea070b2a1e2e3791379c6496f3801a
leonvk <leonvk2000@gmail.com>
2019-06-20 10:41:53 +0200

    fixss

--- src/reader/TitelView.java
+++ src/reader/TitelView.java
@@ -4,9 +4,11 @@ import javax.swing.JLabel;
 
 public class TitelView {
 	
+	public TitelView() {
+	
 	//labels
 	JLabel newsAppLabel = new JLabel("NewsApp");
 	newsAppLabel.setHorizontalAlignment(JLabel.CENTER);
 	add(newsAppLabel);
-
+	}
 }
