b63efb5c4336aa19c85c62197db6072a8c6f8584
leonvk <leonvk2000@gmail.com>
2019-06-20 10:28:00 +0200

    git commit

--- src/reader/NewsApp.java
+++ src/reader/NewsApp.java
@@ -1,6 +1,8 @@
 
 package reader;
 
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,41 +22,34 @@ public class NewsApp {
 }
 
 // Het paneel
-class paneel extends JPanel {
-
-	public paneel() {
-		setLayout(null);
-		// Maak de tekstvakken
-
-		// maak knop
-		JButton categorie1 = new JButton("Voetbal");
-		categorie1.setBounds(920, 200, 100, 40);
-		JButton categorie2 = new JButton("Tennis");
-		categorie2.setBounds(1020, 200, 100, 40);
-		JButton categorie3 = new JButton("Sport Vissen");
-		categorie3.setBounds(920, 240, 100, 40);
-		JButton categorie4 = new JButton("Golf");
-		categorie4.setBounds(1020, 240, 100, 40);
-		JButton categorie5 = new JButton("Gymnastiek");
-		categorie5.setBounds(920, 280, 100, 40);
-		JButton categorie6 = new JButton("Hockey");
-		categorie6.setBounds(1020, 280, 100, 40);
-		// Maak de labels
-		JLabel newsAppLabel = new JLabel("NewsApp");
-		newsAppLabel.setHorizontalAlignment(JLabel.CENTER);
-		newsAppLabel.setBounds(550, 20, 100, 30);
-		JLabel fav = new JLabel("Favorieten");
-		fav.setBounds(1000, 150, 400, 20);
-		// Voeg de componenten toe aan het paneel
-		add(newsAppLabel);
-		add(fav);
-		add(categorie1);
-		add(categorie2);
-		add(categorie3);
-		add(categorie4);
-		add(categorie5);
-		add(categorie6);
-
-	}
-
-}
+//class paneel extends JPanel {
+//
+//	public paneel() {
+//		
+//		
+//		GridLayout experimentLayout = new GridLayout(0,2);
+//		setLayout(experimentLayout);
+//		// Maak de tekstvakken
+//
+//		// maak knop
+//		JButton categorie1 = new JButton("Voetbal");
+//		JButton categorie2 = new JButton("Tennis");
+//		JButton categorie3 = new JButton("Sport Vissen");
+//		JButton categorie4 = new JButton("Golf");
+//		JButton categorie5 = new JButton("Gymnastiek");
+//		JButton categorie6 = new JButton("Hockey");
+//		// Maak de labels
+//		JLabel newsAppLabel = new JLabel("NewsApp");
+//		newsAppLabel.setHorizontalAlignment(JLabel.CENTER);
+//		JLabel fav = new JLabel("Favorieten");
+//		// Voeg de componenten toe aan het paneel
+//		add(newsAppLabel);
+//		add(fav);
+//		add(categorie1);
+//		add(categorie2);
+//		add(categorie3);
+//		add(categorie4);
+//		add(categorie5);
+//		add(categorie6);
+//
+//	}
