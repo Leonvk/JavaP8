e17acd2f8d323fe1121b1a93def21cd45ba2f6df
JurgenJongetjes <39690819+JurgenJongetjes@users.noreply.github.com>
2019-06-20 10:36:14 +0200

    eee
    eee

--- src/reader/NewsApp.java
+++ src/reader/NewsApp.java
@@ -1,8 +1,6 @@
 
 package reader;
 
-import java.awt.GridLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -21,7 +19,6 @@ public class NewsApp {
 	}
 }
 
-
 class paneel extends JPanel {
 
 	public paneel() {
@@ -35,9 +32,7 @@ class paneel extends JPanel {
 		JButton categorie6 = new JButton("Hockey");
 		JLabel newsAppLabel = new JLabel("NewsApp");
 		newsAppLabel.setHorizontalAlignment(JLabel.CENTER);
-		newsAppLabel.setBounds(550, 20, 100, 30);
 		JLabel fav = new JLabel("Favorieten");
-		fav.setBounds(1000, 150, 400, 20);
 		
 		add(newsAppLabel);
 		add(fav);
@@ -51,37 +46,3 @@ class paneel extends JPanel {
 	}
 
 }
-
-// Het paneel
-//class paneel extends JPanel {
-//
-//	public paneel() {
-//
-//
-//		GridLayout experimentLayout = new GridLayout(0,2);
-//		setLayout(experimentLayout);
-//		// Maak de tekstvakken
-//
-//		// maak knop
-//		JButton categorie1 = new JButton("Voetbal");
-//		JButton categorie2 = new JButton("Tennis");
-//		JButton categorie3 = new JButton("Sport Vissen");
-//		JButton categorie4 = new JButton("Golf");
-//		JButton categorie5 = new JButton("Gymnastiek");
-//		JButton categorie6 = new JButton("Hockey");
-//		// Maak de labels
-//		JLabel newsAppLabel = new JLabel("NewsApp");
-//		newsAppLabel.setHorizontalAlignment(JLabel.CENTER);
-//		JLabel fav = new JLabel("Favorieten");
-//		// Voeg de componenten toe aan het paneel
-//		add(newsAppLabel);
-//		add(fav);
-//		add(categorie1);
-//		add(categorie2);
-//		add(categorie3);
-//		add(categorie4);
-//		add(categorie5);
-//		add(categorie6);
-//
-//	}
-
